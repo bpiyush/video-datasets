aaa5981ecac51043966460dc7102fe81333b673e
Piyush Bagad <piyushnbagad11@gmail.com>
2022-04-27 10:34:09 +0200

    Adds VideoItem class and WIP notebook for DiDeMo

---
+++ utils/video_item.py
@@ -0,0 +1,147 @@
+"""
+Class to handle a single video object.
+"""
+
+import numpy as np
+import torch
+import torchvision
+import av
+import random
+
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+from video_transforms import ClipToTensor, Resize
+
+
+
+def av_load_video(container, video_fps=None, start_time=0, duration=None):
+    """Helper function to load video."""
+    video_stream = container.streams.video[0]
+    _ss = video_stream.start_time * video_stream.time_base
+    _dur = video_stream.duration * video_stream.time_base
+    _ff = _ss + _dur
+    _fps = video_stream.average_rate
+
+    if video_fps is None:
+        video_fps = _fps
+
+    if duration is None:
+        duration = _ff - start_time
+
+    # Figure out which frames to decode
+    iterator = np.arange(start_time, min(start_time + duration - 0.5/_fps, _ff), 1./video_fps)
+    outp_times = [t for t in iterator][:int(duration*video_fps)]
+    outp_vframes = [int((t - _ss) * _fps) for t in outp_times]
+    start_time = outp_vframes[0] / float(_fps)
+
+    # Fast forward
+    container.seek(int(start_time * av.time_base))
+
+    # Decode snippet
+    frames = []
+    for frame in container.decode(video=0):
+        if len(frames) == len(outp_vframes):
+            break   # All frames have been decoded
+        frame_no = frame.pts * frame.time_base * _fps
+        if frame_no < outp_vframes[len(frames)]:
+            continue    # Not the frame we want
+
+        # Decode
+        pil_img = frame.to_image()
+        while frame_no >= outp_vframes[len(frames)]:
+            frames += [pil_img]
+            if len(frames) == len(outp_vframes):
+                break   # All frames have been decoded
+
+    return frames, video_fps, start_time
+
+
+class VideoItem:
+    """Class to handle a single video object."""
+
+    def __init__(self, video_path: str) -> None:
+        assert isinstance(video_path, str)
+        assert video_path.endswith(".mp4") or video_path.endswith(".mkv")
+        self.video_path = video_path
+
+        self.video_transform = [
+            Resize(270),
+            ClipToTensor(),
+        ]
+    
+    def _get_time_lims(self, video_ctr):
+        """Returns start and end time of the video."""
+        video_st, video_ft = None, None
+
+        video_stream = video_ctr.streams.video[0]
+        tbase = video_stream.time_base
+        video_st = video_stream.start_time * tbase
+        video_dur = video_stream.duration * tbase
+        video_ft = video_st + video_dur
+
+        return video_st, video_ft
+    
+    def _sample_snippet(self, video_container, video_clip_duration):
+        """
+        Samples a snippet of duration `video_clip_duration` (secs) from
+        the range [t_{s}, t_{f}] within the video. By default, the range
+        is [0, video_duration].
+        """
+        video_st, video_ft = self._get_time_lims(video_container)
+        video_duration = video_ft - video_st
+
+        if video_clip_duration > video_duration:
+            return 0., video_duration
+        else:
+            # video_clip_duration <= video_duration
+            min_d, max_d = video_clip_duration, min(video_clip_duration, video_duration)
+            duration = random.uniform(min_d, max_d)
+            sample_ss_v = random.uniform(video_st, video_ft - duration)
+            return sample_ss_v, duration
+
+    def _get_clip(self, video_ctr, video_start_time, video_clip_duration):
+        
+        sample = {}
+
+        frames, fps, start_time = av_load_video(
+            video_ctr,
+            video_fps=None,
+            start_time=video_start_time,
+            duration=video_clip_duration,
+        )
+        if self.video_transform is not None:
+            for t in self.video_transform:
+                frames = t(frames)
+
+        sample['frames'] = frames
+        sample['fps'] = fps.numerator / fps.denominator
+        sample['start_time'] = start_time
+        sample['duration'] = video_clip_duration
+
+        return sample
+
+    def load_video(self, video_path):
+        """Loads the container of the video."""
+        video_container = av.open(video_path)
+        return video_container
+
+
+if __name__ == "__main__":
+    # video_path = "/var/scratch/pbagad/datasets/activitynet-1.3/v1-3/test/v_zVRajE-LL4I.mp4"
+    video_path = "/ssd/pbagad/datasets/DiDeMo/videos/57959349@N00_3017931116_62f4d0a592.mov.mp4"
+    print("Loading video at {}".format(video_path))
+    video_item = VideoItem(video_path)
+
+    video_container = video_item.load_video(video_path)
+
+    video_st, video_ft = video_item._get_time_lims(video_container)
+    video_duration = float(video_ft - video_st)
+    print("The video duration is: {}".format(video_duration))
+
+    sample_ss_v, duration = video_item._sample_snippet(video_container, 10.)
+    clip = video_item._get_clip(video_container, sample_ss_v, duration)
+
+    clip = video_item._get_clip(video_container, 2.31, 1.)
+    assert clip["frames"].shape == torch.Size([3, 29, 270, 360])
+    assert clip["duration"] == 1.
+    assert clip["start_time"] == 2.3023000000000002
---
+++ utils/video_transforms.py
@@ -0,0 +1,141 @@
+"""
+Video transforms.
+"""
+import numbers
+import PIL
+from PIL import Image
+import numpy as np
+import torch
+# import torch.nn.functional as F
+
+import functional as F
+
+
+class Resize(object):
+    """Resizes a list of (H x W x C) numpy.ndarray to the final size
+    The larger the original image is, the more times it takes to
+    interpolate
+    Args:
+    interpolation (str): Can be one of 'nearest', 'bilinear'
+    defaults to nearest
+    size (tuple): (widht, height)
+    """
+
+    def __init__(self, size, interpolation='nearest'):
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, clip):
+        resized = F.resize_clip(
+            clip, self.size, interpolation=self.interpolation)
+        return resized
+
+
+class CenterCrop(object):
+    """Extract center crop at the same location for a list of images
+    Args:
+    size (sequence or int): Desired output size for the
+    crop in format (h, w)
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            size = (size, size)
+
+        self.size = size
+
+    def __call__(self, clip):
+        """
+        Args:
+        img (PIL.Image or numpy.ndarray): List of images to be cropped
+        in format (h, w, c) in numpy.ndarray
+        Returns:
+        PIL.Image or numpy.ndarray: Cropped list of images
+        """
+        h, w = self.size
+        if isinstance(clip[0], np.ndarray):
+            im_h, im_w, im_c = clip[0].shape
+        elif isinstance(clip[0], PIL.Image.Image):
+            im_w, im_h = clip[0].size
+        else:
+            raise TypeError('Expected numpy.ndarray or PIL.Image' +
+                            'but got list of {0}'.format(type(clip[0])))
+        if w > im_w or h > im_h:
+            error_msg = (
+                'Initial image size should be larger then '
+                'cropped size but got cropped sizes : ({w}, {h}) while '
+                'initial image is ({im_w}, {im_h})'.format(
+                    im_w=im_w, im_h=im_h, w=w, h=h))
+            raise ValueError(error_msg)
+
+        x1 = int(round((im_w - w) / 2.))
+        y1 = int(round((im_h - h) / 2.))
+        cropped = F.crop_clip(clip, y1, x1, h, w)
+
+        return cropped
+
+
+
+def convert_img(img):
+    """Converts (H, W, C) numpy.ndarray to (C, W, H) format
+    """
+    if len(img.shape) == 3:
+        img = img.transpose(2, 0, 1)
+    if len(img.shape) == 2:
+        img = np.expand_dims(img, 0)
+    return img
+
+
+class ClipToTensor(object):
+    """Convert a list of m (H x W x C) numpy.ndarrays in the range [0, 255]
+    to a torch.FloatTensor of shape (C x m x H x W) in the range [0, 1.0]
+    """
+
+    def __init__(self, channel_nb=3, div_255=False, numpy=False):
+        self.channel_nb = channel_nb
+        self.div_255 = div_255
+        self.numpy = numpy
+
+    def __call__(self, clip):
+        """
+        Args: clip (list of numpy.ndarray): clip (list of images)
+        to be converted to tensor.
+        """
+        # Retrieve shape
+        if isinstance(clip[0], np.ndarray):
+            h, w, ch = clip[0].shape
+            assert ch == self.channel_nb, 'Got {0} instead of 3 channels'.format(
+                ch)
+        elif isinstance(clip[0], Image.Image):
+            w, h = clip[0].size
+        else:
+            raise TypeError('Expected numpy.ndarray or PIL.Image\
+            but got list of {0}'.format(type(clip[0])))
+
+        np_clip = np.zeros([self.channel_nb, len(clip), int(h), int(w)])
+
+        # Convert
+        for img_idx, img in enumerate(clip):
+            if isinstance(img, np.ndarray):
+                pass
+            elif isinstance(img, Image.Image):
+                img = np.array(img, copy=False)
+            else:
+                raise TypeError('Expected numpy.ndarray or PIL.Image\
+                but got list of {0}'.format(type(clip[0])))
+            img = convert_img(img)
+            np_clip[:, img_idx, :, :] = img
+        if self.numpy:
+            if self.div_255:
+                np_clip = np_clip / 255
+            return np_clip
+
+        else:
+            tensor_clip = torch.from_numpy(np_clip)
+            if self.div_255:
+                if not isinstance(tensor_clip, torch.FloatTensor):
+                    tensor_clip = tensor_clip.float()
+
+                tensor_clip = tensor_clip.div(255)
+            return tensor_clip
+
