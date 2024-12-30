5785b60937ef92219ff4f376797fde5b19758f07
Piyush Bagad <piyushnbagad11@gmail.com>
2024-12-30 22:20:50 +0530

    Create download.sh

---
+++ vatex/download.sh
@@ -0,0 +1,3 @@
+wget https://eric-xw.github.io/vatex-website/data/vatex_validation_v1.0.json
+wget https://eric-xw.github.io/vatex-website/data/vatex_public_test_english_v1.1.json
+wget https://eric-xw.github.io/vatex-website/data/vatex_training_v1.0.json
